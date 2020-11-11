fda8df5261c7f916a8d80c9808246690a2df4b6a
Dan Brody <daniel@brody.ca>
2020-11-11 12:55:43 -0500

    Updates

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '4fc0e5205f143646f7b099abbd3649b0'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'df2f5b74-d3a5-47d8-bd53-50a19df761e8'
+};
